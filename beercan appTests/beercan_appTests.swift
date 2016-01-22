27ed459c5f5d4bcf896a2276617a003b20bbaffa
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2016-01-22 21:10:50 +0200

    Initial Commit

---
+++ beercan appTests/beercan_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  beercan_appTests.swift
+//  beercan appTests
+//
+//  Created by  on 1/22/16.
+//  Copyright © 2016 beercan. All rights reserved.
+//
+
+import XCTest
+@testable import beercan_app
+
+class beercan_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
